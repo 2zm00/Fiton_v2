f96bcc64287e45cc39c6580f34ae7158d596394c
kim <ghkddus04@gmail.com>
2025-01-21 17:37:01 +0900

    feat:review 모델

--- backend/django/reviews/models.py
+++ backend/django/reviews/models.py
@@ -6,13 +6,13 @@ from lessons.models import Lesson
 # 리뷰 모델
 class Review(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='reviews', verbose_name="수강생")
-    lesson_review = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='reviews',verbose_name="수업")
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='reviews',verbose_name="수업")
     rating = models.IntegerField(verbose_name="평점")
     comment = models.TextField(verbose_name="리뷰 내용")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="작성 시간")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정 시간")
 
     def __str__(self):
-        return f"{self.member.user.name} - {self.lesson_review.name} ({self.rating})"
+        return f"{self.member.user.name} - {self.lesson.name} ({self.rating})"
             
 
